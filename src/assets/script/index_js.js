d29e3d5c163565c656a44fbf2da160d25dc5ea80
aav9060 <aav9060@rit.edu>
2024-12-21 22:52:24 -0500

    start for nav bar

---
+++ src/assets/script/index_js.js
@@ -0,0 +1,18 @@
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
+
+function navResponsive() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
