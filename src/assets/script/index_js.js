b88e802c62c95a9be9432123cd1a454eccba16c7
aav9060 <aav9060@rit.edu>
2024-12-30 23:41:10 -0500

    Content for nav bar added, needs to be sorted through & restylized

--- src/assets/script/index_js.js
+++ src/assets/script/index_js.js
@@ -23,6 +23,14 @@ function scrollFunction() {
   }
 }
 
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
+
+
 function navResponsive() {
   var x = document.getElementById("navbar");
   var y = document.getElementById("dropdown");
